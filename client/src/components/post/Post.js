507650e261d3922bcc724d802e253c6a6a6095bf
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2022-01-02 17:53:45 -0500

    test aws1

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -13,8 +13,8 @@ export default function Post({ post }) {
     //localhost/4000 is the local server or api to store images in small projects           
 
     //const PF = 'https://process.env.AWS_BUCKET_NAME.s3.amazonaws.com/' 
-    //const PF = 'https://exdblog-app.s3.amazonaws.com/'  //production - photos stored in aws3
-    const PF = "http://localhost:4000/images/"  //for testing one when photos in server/images  
+    const PF = 'https://exdblog-app.s3.amazonaws.com/'  //production - photos stored in aws3
+    //const PF = "http://localhost:4000/images/"  //for testing one when photos in server/images  
     //const PF = "https://expansivedesigns.com/exdblog/images/"
     //const PF = "https://exdblog.herokuapp.com/images/"  //temp location of stored images - PF = public folder  - does not work for heroku  
 
