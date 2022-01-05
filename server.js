68b91c6407f16d5e585a28249e7a837590ce601d
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2022-01-05 06:31:14 -0500

    updated aws3 configs

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 4000
 
-mongoose.connect(process.env.MONGODB_URI, {
+const dbURL = "mongodb://mongo:27017/exdblog"
+
+mongoose.connect(process.env.MONGODB_URI || dbURL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
@@ -35,86 +37,85 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 // //*** AWS3 test local code - start ***
 
-//MULTER
-//multer is used in the test to upload images directly in the api/server folder in /images
-//in production we can use firebase, aws3 to upload images and 
-//we can create a separate route to upload (s3.js) instead of in server.js
+// //MULTER
+// //multer is used in the test to upload images directly in the api/server folder in /images
+// //in production we can use firebase, aws3 to upload images and 
+// //we can create a separate route to upload (s3.js) instead of in server.js
 
-const multer = require("multer") 
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {  //destination to store image - cb takes care of errors
-        cb(null, "images")  //if error then null, if no errors files go in images/server folder
-    },
-    filename: (req, file, cb) => { //filename - the name from name field in client
-        cb(null, req.body.name) //if error then null, if no error name taken from req.body.name on client
-    }, 
-}) 
-
-const upload = multer({ storage: storage })  //config to upload of storage, created above
-
-app.post("/api/upload", upload.single("file"), (req, res) => {  //uploading file named "file"
-    const file = req.file  //FE requests the file
-    console.log(file)
-    res.send(file)   //FE sends the file back to the client
-    req.status(200).json("File has been uploaded.") //  
-})
-
-//*** AWS3 test local code - end ***    
-
-// //step 2.  Upload images from client-side to AWS3
-// const aws = require('aws-sdk')
-
-// const bucket = process.env.AWS_BUCKET_NAME
-// const region = process.env.AWS_BUCKET_REGION
-// const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-// const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY  
-
-// const s3 = new aws.S3({
-//     accessKeyId,
-//     secretAccessKey,
-//     region,
-// })  
-    
 // const multer = require("multer") 
-// const multerS3 = require("multer-s3")   
 
-// const fileFilter = (req, file, cb) => {
-//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-//         cb(null, true)
-//     } else {
-//         cb(null, false)
-//     }
-// }
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {  //destination to store image - cb takes care of errors
+//         cb(null, "images")  //if error then null, if no errors files go in images/server folder
+//     },
+//     filename: (req, file, cb) => { //filename - the name from name field in client
+//         cb(null, req.body.name) //if error then null, if no error name taken from req.body.name on client
+//     }, 
+// }) 
 
-// const upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: process.env.AWS_BUCKET_NAME,            
-//         acl: "public-read",                        
-//         metadata: function (req, file, cb) {
-//         cb(null, {fieldName: file.fieldname});
-//         },
-//         fileFilter: fileFilter,
-//         limits: {
-//             fileSize: 1024 * 1024 * 5 // only allows 5 MB files
-//         },    
-//         key: function (req, file, cb) {  //name of file taken from originalname and saved as key
-//             console.log(file)
-//             cb(null, file.originalname )  //if no original name then error is null
-//             //cb(null, Date.now() + '-' + file.originalname)
-//             //cb(null, Date.now().toString())
-            
-//         }
-//     })
-// })
+// const upload = multer({ storage: storage })  //config to upload of storage, created above
 
-// app.post("/api/upload", upload.single("file"), (req, res) => {  //update file to aws3 based on multer
+// app.post("/api/upload", upload.single("file"), (req, res) => {  //uploading file named "file"
 //     const file = req.file  //FE requests the file
 //     console.log(file)
 //     res.send(file)   //FE sends the file back to the client
-//     //req.status(200).json("File has been uploaded.") //
+//     req.status(200).json("File has been uploaded.") //  
 // })
+
+// //*** AWS3 test local code - end ***    
+
+//step 2.  Upload images from client-side to AWS3
+const aws = require('aws-sdk')
+
+const bucket = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY  
+
+const s3 = new aws.S3({
+    accessKeyId,
+    secretAccessKey,
+    region,
+})  
+    
+const multer = require("multer") 
+const multerS3 = require("multer-s3")   
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME,            
+        acl: "public-read",                        
+        metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname});
+        },
+        fileFilter: fileFilter,
+        limits: {
+            fileSize: 1024 * 1024 * 5 // only allows 5 MB files
+        },    
+        key: function (req, file, cb) {  //name of file taken from originalname and saved as key
+            console.log(file)
+            cb(null, file.originalname )  //if no original name then error is null
+            //cb(null, Date.now() + '-' + file.originalname)
+            //cb(null, Date.now().toString())            
+        }
+    })
+})
+
+app.post("/api/upload", upload.single("file"), (req, res) => {  //update file to aws3 based on multer
+    const file = req.file  //FE requests the file
+    console.log(file)
+    res.send(file)   //FE sends the file back to the client
+    //req.status(200).json("File has been uploaded.") //
+})
 
 //endpoints
 app.use("/api/auth", authRoute)   
