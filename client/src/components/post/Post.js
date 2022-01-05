b21c0153b16aa7a49d29a8a5d4bc0c7c1ccf06ac
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2022-01-05 06:42:19 -0500

    updated aws config

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -12,8 +12,8 @@ dotenv.config()
 export default function Post({ post }) {  
     //localhost/4000 is the local server or api to store images in small projects           
 
-    //const PF = 'https://process.env.AWS_BUCKET_NAME.s3.amazonaws.com/' 
-    const PF = 'https://exdblog-app.s3.amazonaws.com/'  //production - photos stored in aws3
+    const PF = 'https://process.env.AWS_BUCKET_NAME.s3.amazonaws.com/' 
+    //const PF = 'https://exdblog-app.s3.amazonaws.com/'  //production - photos stored in aws3
     //const PF = "http://localhost:4000/images/"  //for testing one when photos in server/images  
     //const PF = "https://expansivedesigns.com/exdblog/images/"
     //const PF = "https://exdblog.herokuapp.com/images/"  //temp location of stored images - PF = public folder  - does not work for heroku  
